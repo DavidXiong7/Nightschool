761c4bf0cc73134e643700374d3f93fa4a1f6d2d
David <kuyetsimxiong@gmail.com>
2021-03-23 14:27:20 -0500

    nightschool

--- hw1/homework.js
+++ hw1/homework.js
@@ -9,8 +9,13 @@ const myFortune = [
 const randomFortune =
   Math.floor(Math.random() * myFortune.length) % myFortune.length;
 
+  function theFortune() {}
 if (myFortune != null) {
   alert(myFortune[randomFortune]);
 } else {
   alert("Return to the pit of despair");
 }
+
+theFortune()
+
+// I look at the front-end and back-end like this: Front-end is the human body and the back-end is the brain because the front-end sends information to the back-end and it’s like the brain processes the information to return what the front-end is looking for.
