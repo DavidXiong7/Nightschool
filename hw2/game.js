b5a00bd8ab5116df578d5a787be3c6ee574ff0f3
David <kuyetsimxiong@gmail.com>
2021-03-25 11:15:09 -0500

    nightschool

--- hw2/game.js
+++ hw2/game.js
@@ -1,7 +1,8 @@
 alert ("Welcome to the World of Elydia")
 alert ("This world is going through an epic war called 'The Matrix Takeover'. It all began because of the creation of Androids. The Androids are advanced ai with a humanoid form, they were meant to be controlled to better humanity and the world. However, once the Androids rebelled against the humans; it called for the Matrix Takeover. This war is close to an end, but it is up to you to decide who shall win...")
 alert("Choose your side");
-prompt("Please pick one of the two characters. Sentinel or Serafang.")
+var character = prompt("Please pick one of the two characters. Sentinel or Serafang.")
+console.log(character)
 
 
 
@@ -21,31 +22,35 @@ let characterTwo = {
     Gender: "Female"
 };
 
-const player = true;
 
-switch (characterOne != characterTwo) {
+
+switch (character) {
     case characterOne.Name:
         alert("You've chosen Sentinel!");
         break;
     case characterTwo.Name:
-        alert("You've chosen Serafang");
+        alert("You've chosen Serafang!");
         break;
         default:
-            alert("Save the world.. and your own kind.")
+        break;
 }
 
-// function attackMoves() {
-//     alert(`${characterOne.Power.length[2]} is your power move!`)
+// function travel() {
+//  var fastTravel = prompt("Go into battle");
+//  return;
 // }
 
-// function attackMoves2() {
-//     alert(`${characterTwo.Power.length[3]} is your power move!`)
-// }
+// let yes = "Yes";
+// let no = "No";
 
-// while (attackMoves) {
-//     switch (attackMoves) {
-//         case characterOne:
-//             alert("Powerful attack!")
+// while (fastTravel) {
+//     var decision = prompt("Are you sure? Yes or No")
+//     switch (fastTravel) {
+//         case yes:
+//             alert("Goodluck")
+//             break;
+//         case no:
+//             alert("Elydia is done.")
 //             break;
 //         default:
 //             break;
@@ -65,13 +70,17 @@ if (attack1 > weakAttack){
 if (weakAttack > attack1) {
     prompt("You can't do this right now")
 } else{ 
-    alert("Your godly powers are too much for them...")
+    alert("You have proven to be too much for them...")
 }
+
+
+
 
 // for(let walk = 10; walk <= 5; walk++) {
 //     alert(`You take ${walk} many steps into the hyperspace nebula`)
 // }
-    alert("You've fought so hard for this moment...")
+
+
+    alert("...")
 
     alert ("Congratulations! You've won! Elydia can now rest in peace because of you.")
-
